e5fc16b8246acfbcfd9d33cbd669420530a9bd5c
Raghavankutty <raghavankutty35@gmail.com>
2024-06-11 19:13:45 +0530

    bg_colour

--- alien_invasion.py
+++ alien_invasion.py
@@ -7,8 +7,11 @@ class AlienInversion:
         """initialize the game,and create game resources"""
         pygame.init()
         self.clock = pygame.time.Clock()
-
         self.screen=pygame.display.set_mode((1200, 800))
+        pygame.display.set_caption("Alien Inversion")
+
+        #set the background colour.
+        self.bg_colour = (230,230,230)
 
     def run_game(self):
         """start the main loop for the game."""
@@ -18,6 +21,9 @@ class AlienInversion:
                 if event.type==pygame.QUIT:
                     sys.exit()
 
+            #redraw the screen during each pass through the loop.
+            self.screen.fill(self.bg_colour)
+            
             #make the most recently drawn screen visible.
             pygame.display.flip()
             self.clock.tick(60)
