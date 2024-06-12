58ef03e0af63d1fb5e7a5dcd86c814921f27988e
Raghavankutty <raghavankutty35@gmail.com>
2024-06-12 21:27:22 +0530

    update screen

--- alien_invasion.py
+++ alien_invasion.py
@@ -23,20 +23,22 @@ class AlienInversion:
         """start the main loop for the game."""
         while True:
             self._check_events()
-
-            #redraw the screen during each pass through the loop.
-            self.screen.fill(self.settings.bg_colour)
-            self.ship.blitme()
-
-            #make the most recently drawn screen visible.
-            pygame.display.flip()
+            self._update_screen()
             self.clock.tick(60)
 
     def _check_events(self):
-        #watch for keyboard and mouse events .
+        #respond for keyboard and mouse events .
                 for event in pygame.event.get():
                     if event.type==pygame.QUIT:
                         sys.exit()
+                    
+    def _update_screen(self):
+         #update the screen during each pass through the loop.
+            self.screen.fill(self.settings.bg_colour)
+            self.ship.blitme()
+
+            pygame.display.flip()
+
 
         
 if __name__=='__main__':
