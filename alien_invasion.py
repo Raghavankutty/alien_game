70350943a17452322bd8a97f337e183d321b6c10
Raghavankutty <raghavankutty35@gmail.com>
2024-06-12 22:56:51 +0530

    continous movement

--- alien_invasion.py
+++ alien_invasion.py
@@ -23,6 +23,7 @@ class AlienInversion:
         """start the main loop for the game."""
         while True:
             self._check_events()
+            self.ship.update()
             self._update_screen()
             self.clock.tick(60)
 
@@ -32,8 +33,11 @@ class AlienInversion:
                     if event.type==pygame.QUIT:
                         sys.exit()
                     elif event.type  == pygame.KEYDOWN:
-                         #move the ship to the right.
-                         self.ship.rect.x += 1
+                        if event.key == pygame.K_RIGHT:
+                            self.ship.moving_right = True
+                        elif event.type == pygame.KEYUP:
+                             if event.key == pygame.K_RIGHT:
+                                  self.ship.moving_right = False
                     
     def _update_screen(self):
          #update the screen during each pass through the loop.
