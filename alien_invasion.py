1845a2fa7fef7e49bdb2c755604f41c8384bf35f
Raghavankutty <raghavankutty35@gmail.com>
2024-06-15 16:18:16 +0530

    alien fleet

--- alien_invasion.py
+++ alien_invasion.py
@@ -81,9 +81,18 @@ class AlienInversion:
 
     def _create_fleet(self):
         """create the fleet of aliens"""
-        #make an alien
+        #make an alien and keep adding aliens untill there's no room left
+        #spaceing between aliens is one alien width
         alien = Alien(self)
-        self.aliens.add(alien)
+        alien_width = alien.rect.width
+
+        current_x = alien_width
+        while current_x < (self.settings.screen_width ):
+            new_alien =  Alien(self)
+            new_alien.x = current_x
+            new_alien.rect.x = current_x
+            self.aliens.add(new_alien)
+            current_x += 2 * alien_width
 
     def _update_screen(self):
          #update the screen during each pass through the loop.
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,5 +1,1 @@
-    def _check_events(self):
-        #watch for keyboard and mouse events .
-                for event in pygame.event.get():
-                    if event.type==pygame.QUIT:
-                        sys.exit()
+- 2 * alien_width
